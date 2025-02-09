185f1eb07f9b99021a19dd79c54a59e83748a678
trngngy <nguyenhattruong6200@gmail.com>
2025-02-09 15:35:05 +0700

    AppointmentAPI

---
+++ VaccinaCare.API/Controllers/AppointmentController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VaccinaCare.Application.Interface;
+using VaccinaCare.Application.Interface.Common;
+using VaccinaCare.Application.Ultils;
+using VaccinaCare.Domain.DTOs.AppointmentDTOs;
+namespace VaccinaCare.API.Controllers;
+using VaccinaCare.Repository.Commons;
+
+[ApiController]
+    [Route("api/appointment")]
+
+    public class AppointmentController : ControllerBase
+    {
+      private readonly IAppointmentService _appointmentService;
+      private readonly ILoggerService _logger;
+    public AppointmentController(IAppointmentService appointmentService, ILoggerService logger)
+    {
+        _appointmentService = appointmentService;
+        _logger = logger;
+    }
+    [HttpPost]
+    [Authorize]
+    [ProducesResponseType(typeof(ApiResult<CreateAppointmentDto>), 200)]
+    [ProducesResponseType(typeof(ApiResult<object>), 400)]
+    [ProducesResponseType(typeof(ApiResult<object>), 500)]
+    public async Task<IActionResult> CreateAppointment([FromBody] CreateAppointmentDto createAppointmentDto)
+    {
+        try
+        {
+            _logger.Info("Received request to create appointment.");
+
+            if (createAppointmentDto == null)
+            {
+                _logger.Warn("CreateAppointmentDTO is null.");
+                return BadRequest(new ApiResult<object>
+                {
+                    IsSuccess = false,
+                    Message = "Invalid data. Appointment is required."
+                });
+
+            }
+            var appointment = await _appointmentService.CreateAppointment(createAppointmentDto);
+
+            _logger.Success($"Appointment information created successfully.");
+
+            return Ok(new ApiResult<CreateAppointmentDto>
+            {
+                IsSuccess = true,
+                Message = "Appointment information created successfully.",
+                Data = appointment
+            });
+        }
+        catch (Exception ex) 
+        {
+            _logger.Error($"Error while creating appointment information: {ex.Message}");
+            return StatusCode(500, new ApiResult<object>
+            {
+                IsSuccess = false,
+                Message = "An error occurred while creating the appointment information. Please try again later."
+            });
+        }
+    }
+    [HttpGet]
+    [Authorize]
+    [ProducesResponseType(typeof(ApiResult<Pagination<CreateAppointmentDto>>), 200)]
+    [ProducesResponseType(typeof(ApiResult<object>), 400)]
+    [ProducesResponseType(typeof(ApiResult<object>), 500)]
+    public async Task<IActionResult> GetAppointmentByParent([FromQuery] PaginationParameter paginatio)
+    {
+        try
+        {
+            _logger.Info("Received request to get appointment list.");
+
+            var appointment = await _appointmentService.GetAppointmentByParent(paginatio);
+
+            _logger.Success($"Fetched {appointment.Count} appointment successfully.");
+
+            return Ok(new ApiResult<Pagination<CreateAppointmentDto>>
+            {
+                IsSuccess = true,
+                Message = "Appointment list retrieved successfully.",
+                Data = appointment
+            });
+        }
+        catch (Exception ex)
+        {
+            _logger.Error($"Error while fetching appointment: {ex.Message}");
+            return StatusCode(500, new ApiResult<object>
+            {
+                IsSuccess = false,
+                Message = "An error occurred while retrieving the appointment list. Please try again later."
+            });
+        }
+    }
+}
+
---
+++ VaccinaCare.Application/Service/AppointmentService.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaccinaCare.Application.Interface;
+using VaccinaCare.Application.Interface.Common;
+using VaccinaCare.Application.Service.Common;
+using VaccinaCare.Domain.DTOs.AppointmentDTOs;
+using VaccinaCare.Domain.Entities;
+using VaccinaCare.Repository.Commons;
+using VaccinaCare.Repository.Interfaces;
+
+namespace VaccinaCare.Application.Service
+{
+    public class AppointmentService : IAppointmentService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILoggerService _logger;
+        private readonly IClaimsService _claimsService;
+
+        public AppointmentService(IUnitOfWork unitOfWork, ILoggerService loggerService, IClaimsService claimsService)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = loggerService;
+            _claimsService = claimsService;
+        }
+
+        public async Task<CreateAppointmentDto> CreateAppointment(CreateAppointmentDto createAppointmentDto)
+        {
+            _logger.Info("Starting to create a new appointment.");
+            try
+            {
+                if (createAppointmentDto == null)
+                {
+                    _logger.Error("Appointment creation failed: Input data is null.");
+                    throw new ArgumentNullException(nameof(createAppointmentDto));
+                }
+                if (createAppointmentDto.AppointmentDate < DateTime.UtcNow)
+                {
+                    _logger.Error("Appointment creation failed: Date is in the past.");
+                    throw new ArgumentException("Appointment date must be in the future.");
+                }
+
+                var appointmentEntity = new Appointment
+                {
+                    ParentId = createAppointmentDto.ParentId,
+                    ChildId = createAppointmentDto.ChildId,
+                    AppointmentDate = createAppointmentDto.AppointmentDate,
+                    VaccineType = createAppointmentDto.VaccineType,
+                    TotalPrice = createAppointmentDto.TotalPrice,
+                    PolicyId = createAppointmentDto.PolicyId,
+                    Notes = createAppointmentDto.Notes,
+                    Confirmed = createAppointmentDto.Confirmed,
+                    AppointmentsVaccines = new List<AppointmentsVaccine>()
+                };
+                foreach (var vaccineId in createAppointmentDto.AppointmentsVaccines)
+                {
+                    appointmentEntity.AppointmentsVaccines.Add(new AppointmentsVaccine
+                    {
+                        AppointmentId = appointmentEntity.Id,
+                        VaccineId = vaccineId
+                    });
+                }
+                await _unitOfWork.AppointmentRepository.AddAsync(appointmentEntity);
+                await _unitOfWork.SaveChangesAsync();
+
+                _logger.Info($"Appointment {appointmentEntity.Id} created successfully.");
+                return createAppointmentDto;
+            }
+            catch (Exception ex) 
+            {
+                _logger.Error($"An error occurred while creating the appointment. Error: {ex.Message}");
+                throw;
+            }
+        }
+
+        public Task<CreateAppointmentDto> DeleteAppointment(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Pagination<CreateAppointmentDto>> GetAppointmentByParent(PaginationParameter pagination)
+        {
+            try
+            {
+                Guid parentId = _claimsService.GetCurrentUserId;
+
+                _logger.Info(
+                            $"Fetching appointments for parent {parentId} with pagination: Page {pagination.PageIndex}, Size {pagination.PageSize}");
+
+                var query = _unitOfWork.AppointmentRepository.GetQueryable()
+                    .Where(a => a.ParentId == parentId);
+
+                int totalAppointments = await query.CountAsync();
+
+                var appointments = await query
+                    .OrderByDescending(a => a.AppointmentDate)
+                    .Skip((pagination.PageIndex - 1) * pagination.PageSize)
+                    .Take(pagination.PageSize)
+                    .ToListAsync();
+                if (!appointments.Any())
+                {
+                    _logger.Warn($"No appointments found for parent {parentId} on page {pagination.PageIndex}.");
+                    return new Pagination<CreateAppointmentDto>(new List<CreateAppointmentDto>(), 0, pagination.PageIndex, pagination.PageSize);
+                }
+                _logger.Success(
+            $"Retrieved {appointments.Count} appointments for parent {parentId} on page {pagination.PageIndex}");
+
+                var appointmentDtos = appointments.Select(appointment => new CreateAppointmentDto
+                {
+                    ParentId = appointment.ParentId ?? Guid.Empty,
+                    ChildId = appointment.ChildId ?? Guid.Empty,
+                    AppointmentDate = appointment.AppointmentDate ?? DateTime.UtcNow ,
+                    VaccineType = appointment.VaccineType,
+                    TotalPrice = appointment.TotalPrice ?? 0m,
+                    PolicyId = appointment.PolicyId ?? Guid.Empty,
+                    Notes = appointment.Notes,
+                    Confirmed = appointment.Confirmed ?? false,
+                    AppointmentsVaccines = appointment.AppointmentsVaccines
+                                            .Where(v => v.VaccineId.HasValue)
+                                            .Select(v => v.VaccineId.Value)
+                                            .ToList(),
+                }).ToList();
+                return new Pagination<CreateAppointmentDto>(appointmentDtos, totalAppointments, pagination.PageIndex, pagination.PageSize);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Error while fetching appointments: {ex.Message}");
+                throw new Exception("An error occurred while fetching appointments. Please try again later.");
+            }
+        }
+
+        public Task<CreateAppointmentDto> UpdateAppointment(Guid id, CreateAppointmentDto createAppointmentDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
