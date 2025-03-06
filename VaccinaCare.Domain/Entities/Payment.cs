13bedc6f98adccd856fdffd4cbef79596de0da6c
phuctran362003 <trangiaphuc362003181@gmail.com>
2025-03-06 21:41:47 +0700

    begin payment

--- VaccinaCare.Domain/Entities/Payment.cs
+++ VaccinaCare.Domain/Entities/Payment.cs
@@ -1,14 +1,20 @@
-﻿namespace VaccinaCare.Domain.Entities;
+﻿using VaccinaCare.Domain.Enums;
+
+namespace VaccinaCare.Domain.Entities;
 
 public partial class Payment : BaseEntity
 {
     public Guid? AppointmentId { get; set; }
-
     public decimal? Amount { get; set; }
+    public PaymentStatus? PaymentStatus { get; set; }
 
-    public string? PaymentStatus { get; set; }
+    public PaymentType PaymentType { get; set; } // thanh toán cọc hoặc thanh toán đầy đủ
+    public DateTime? PaymentDate { get; set; } // ngày thanh toán thực tế thành công
 
+    public Guid? PaymentMethodId { get; set; } // khóa ngoại đến PaymentMethod
     public virtual PaymentMethod? PaymentMethod { get; set; }
 
     public virtual ICollection<Invoice> Invoices { get; set; } = new List<Invoice>();
+
+    public virtual ICollection<PaymentTransaction> PaymentTransactions { get; set; } = new List<PaymentTransaction>();
 }
---
+++ VaccinaCare.Domain/Entities/PaymentTransaction.cs
@@ -0,0 +1,20 @@
+using VaccinaCare.Domain.Enums;
+
+namespace VaccinaCare.Domain.Entities;
+
+public partial class PaymentTransaction : BaseEntity
+{
+    public Guid PaymentId { get; set; } // Khóa ngoại đến Payment
+    public string? TransactionId { get; set; } // mã giao dịch trả về từ VNPay
+    public decimal? Amount { get; set; } // số tiền thanh toán
+    public DateTime TransactionDate { get; set; } // thời gian thực hiện giao dịch
+    public string? ResponseCode { get; set; } // mã phản hồi từ cổng thanh toán (00 thành công,...)
+    public string? ResponseMessage { get; set; } // thông điệp từ cổng thanh toán
+    public PaymentTransactionStatus Status { get; set; } // trạng thái giao dịch
+    public string? Note { get; set; } // ghi chú bổ sung nếu cần
+
+    // Navigation properties
+    public virtual Payment Payment { get; set; }
+}
+
+
